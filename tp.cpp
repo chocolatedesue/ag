4a2d4d440d42c23e949f88afb9ce7bdeea9202de
chocolatedesue <chocolatedesue@gmail.com>
2023-07-29 15:01:09 +0800

    fix

--- tp.cpp
+++ tp.cpp
@@ -14,12 +14,13 @@ void solve(){
 
 
 signed main(){
+    ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
     // freopen("in","r",stdin);
 
     int t  ;
     t = 1; 
     // cin >> t;
-    while (t--);
+    while (t--)
     {
         solve();
     }
