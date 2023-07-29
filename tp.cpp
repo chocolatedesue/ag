08adbdc1bd191c628b8cc36755a715ffd54e6c1a
chocolatedesue <chocolatedesue@gmail.com>
2023-07-29 11:05:16 +0800

    init

---
+++ tp.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll ;
+typedef pair<int,int>PII;
+
+
+void solve(){
+
+
+
+}
+
+
+
+signed main(){
+    // freopen("in","r",stdin);
+
+    int t  =1 ; 
+    // cin >> t;
+    while (t--);
+    {
+        solve();
+    }
+
+    return 0;
+}
